c9e81a7499e7455cc4ea36ae3453e26d1bd5a0c3
Wendell Mccaulay Ramos Alves <wendellramos.gft@sulamerica.com.br>
2022-05-18 20:22:41 -0300

    Code to produce message to topic

---
+++ src/main/java/io/github/wendell/kafkaproducerbq/controller/KafkaController.java
@@ -0,0 +1,27 @@
+package io.github.wendell.kafkaproducerbq.controller;
+
+import io.github.wendell.avro.User;
+import io.github.wendell.kafkaproducerbq.producer.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("name") String name, @RequestParam("age") Integer age) {
+        this.producer.sendMessage(new User(name, age));
+    }
+
+}
---
+++ src/test/java/io/github/wendell/kafkaproducerbq/KafkaProducerBqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wendell.kafkaproducerbq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerBqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
